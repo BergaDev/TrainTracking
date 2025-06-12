82c2e3d9ce88dcabe7b1e6899b61356799990c3c
Matthew <55415791+BergaDev@users.noreply.github.com>
2025-06-13 01:18:17 +1000

    Update frontend url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: process.env.API_TEST_URL,
+        target: process.env.API_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
